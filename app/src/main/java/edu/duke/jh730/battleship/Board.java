afe2fcab766e871576267328e180a5c83fa9e5f4
Matty-7 <jh730@duke.edu>
2025-02-03 13:01:33 -0500

    task1

---
+++ app/src/main/java/edu/duke/jh730/battleship/Board.java
@@ -0,0 +1,9 @@
+package edu.duke.jh730.battleship;
+
+/**
+ * This interface represents the basic functionality of a Board in a naval battle game
+ */
+public interface Board {
+  public int getWidth();
+  public int getHeight();
+} 
