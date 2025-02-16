ffd4104caaeb7284cdddf0a574671c9291ebf096
jh730 <jh730@duke.edu>
2025-02-16 17:12:22 -0500

    Revert "GUI version"
    This reverts commit abcd491f9900a3df30be2dd9b1e59c2e7ebaafa8

--- app/src/test/java/edu/duke/jh730/battleship/AppTest.java
+++ app/src/test/java/edu/duke/jh730/battleship/AppTest.java
@@ -1,12 +1,44 @@
 package edu.duke.jh730.battleship;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.StringReader;
+import java.io.BufferedReader;
+
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
-    @Test
-    public void test_app_creation() {
-        App app = new App();
-        assertNotNull(app);
-    }
+  private TextPlayer createTextPlayer(String name, String inputData, ByteArrayOutputStream bytes) {
+    BufferedReader input = new BufferedReader(new StringReader(inputData));
+    PrintStream output = new PrintStream(bytes, true);
+    Board<Character> board = new BattleShipBoard<Character>(10, 20, 'X');
+    V1ShipFactory shipFactory = new V1ShipFactory();
+    return new TextPlayer(name, board, input, output, shipFactory);
+  }
+
+  @Test
+  void test_app_init() {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    TextPlayer p1 = createTextPlayer("A", "B2V\n", bytes);
+    TextPlayer p2 = createTextPlayer("B", "C3H\n", bytes);
+    App app = new App(p1, p2);
+    assertNotNull(app);
+  }
+
+  @Test
+  void test_do_placement_phase() throws IOException {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    String inputData = "A0V\nB1V\nC2V\nD3V\nE4V\nF5V\nG6V\nH7V\nI8V\nJ9V\n";
+    TextPlayer p1 = createTextPlayer("A", inputData, bytes);
+    TextPlayer p2 = createTextPlayer("B", inputData, bytes);
+    App app = new App(p1, p2);
+    app.doPlacementPhase();
+    String output = bytes.toString();
+    assertTrue(output.contains("Player A: you are going to place"));
+    assertTrue(output.contains("Player B: you are going to place"));
+  }
+
 }
